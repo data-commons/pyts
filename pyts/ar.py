4ccf24aa7c6f7f2d9a7e3a68e159b5ef521cebae
Abhishek Gupta <abhishkg@thoughtworks.com>
2017-02-27 14:23:14 +0530

    Implementation for the auto regression basic model is done where we are using stats package for now.

--- pyts/ar.py
+++ pyts/ar.py
@@ -1,6 +1,12 @@
-class AR(object):
+from statsmodels.tsa.ar_model import AR 
+
+class AutoRegression(object):
 	def __init__(self,data_set):
 		self.data_set = data_set
+		self.model = AR(self.data_set)
+		self.model_fit = self.model.fit()
 
 	def predict(self,n_ahead):
-		return [6,7]
+		length = len(self.data_set)
+		predictions = self.model_fit.predict(start=length,end=length+n_ahead-1,dynamic=True)
+		return predictions
--- tests/test_ar.py
+++ tests/test_ar.py
@@ -1,10 +1,12 @@
-from pyts.ar import AR
+from pyts.ar import AutoRegression
+import numpy as np
+import numpy.testing as npt
 import unittest
 
 class TestAR(unittest.TestCase):
 
-    def test_upper(self):
-        ar_model = AR([1,2,3,4,5])
+    def test_should_use_ols_for_auto_regression_and_predict_the_next_two_values_for_given_array(self):
+        ar_model = AutoRegression(np.array([1,2,3,4,5,6,7,8]))
         actual = ar_model.predict(2)
-        expected = [6,7]
-        self.assertEqual(actual,[6,7])
+        expected = np.array([9,10])
+        npt.assert_allclose(actual,expected)
